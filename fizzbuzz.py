bd3bdd4845946e5810d9c4a03499fc6fef1ade13
MattJStellato <94338080+MattJStellato@users.noreply.github.com>
2021-11-14 22:34:48 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
-n = int(input("Welcome to FizzBuzz!\nHow many numbers would you like to count?\n"))
+def main():
+    n = int(input("Welcome to FizzBuzz!\nHow many numbers would you like to count?\n"))
 
-for i in range(1,n+1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print(i)
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
